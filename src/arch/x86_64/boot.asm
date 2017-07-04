ae1a461991a80516e71d3d8bb9d03f2a2d973355
Michael Blockley <mikey@spotify.com>
2017-07-04 17:34:28 +0200

    added some memory usage metrics, increased stack

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -14,6 +14,7 @@ section .text
 bits 32
 start:
     mov esp, stack_top
+    mov edi, ebx
 
     call check_multiboot
     call check_cpuid
@@ -159,5 +160,5 @@ p3_table:
 p2_table:
     resb 4096
 stack_bottom:
-    resb 64
+    resb 4096 * 4
 stack_top:
--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,47 @@
 #![no_std]
 
 extern crate rlibc;
-extern crate volatile;
+extern crate multiboot2;
 extern crate spin;
+extern crate volatile;
 
 #[macro_use]
 mod vga_buffer;
 
 #[no_mangle]
-pub extern fn rust_main() {
-    // ATTENTION: we have a very small stack and no guard page
+pub extern fn rust_main(multiboot_information_address: usize) {
+    let boot_info = unsafe {
+        multiboot2::load(multiboot_information_address)
+    };
 
     vga_buffer::clear_screen();
+
     println!("Hello World{}", "!");
 
-    // deadlock test
-    println!("{}", { println!("inner"); "outer" });
+    // kernel and multiboot sizes
+    let elf_sections_tag = boot_info.elf_sections_tag()
+        .expect("ELF-sections tag expected");
+    let kernel_start = elf_sections_tag.sections().map(|s| s.addr)
+        .min().unwrap();
+    let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size)
+        .max().unwrap();
+    let multiboot_start = multiboot_information_address;
+    let multiboot_end = multiboot_start+ (boot_info.total_size as usize);
+    println!("kernel:");
+    println!("    start: 0x{:x}, length: 0x{:x}",
+             kernel_start, kernel_end - kernel_start);
+    println!("multiboot:");
+    println!("    start: 0x{:x}, length: 0x{:x}",
+             multiboot_start, multiboot_end - multiboot_start);
+
+    // print available memory
+    let memory_map_tag = boot_info.memory_map_tag()
+        .expect("Memory map tag required");
+    println!("memory areas:");
+    for area in memory_map_tag.memory_areas() {
+        println!("    start: 0x{:x}, length: 0x{:x}",
+                 area.base_addr, area.length);
+    }
     loop {}
 }
 
@@ -25,4 +51,9 @@ pub extern fn rust_main() {
 
 #[lang = "panic_fmt"]
 #[no_mangle]
-pub extern fn panic_fmt() -> ! {loop {}}
+pub extern fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str,
+                        line: u32) -> ! {
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("    {}", fmt);
+    loop {}
+}
